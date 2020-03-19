924423d1aa101205cd9ca7e4cbe90d9aca588856
Chaffe <mudrogelov.beled@gmail.com>
2020-03-19 22:03:57 +0300

    feat: implement task solution

--- src/index.js
+++ src/index.js
@@ -1,12 +1,30 @@
-
 exports.min = function min (array) {
-  return 0;
+  let min = 0;
+  
+  if (!array || array.length === 0) return 0;
+
+  for (let i = 0; i < array.length; i++) {
+    if (array[i] < min) min = array[i];
+  }
+
+  return min;
 }
 
 exports.max = function max (array) {
-  return 0;
+  let max = 0;
+
+  if (!array || array.length === 0) return 0;
+
+  for (let i = 0; i < array.length; i++) {
+    if (array[i] > max) max = array[i];
+  }
+  
+  return max;
 }
 
 exports.avg = function avg (array) {
-  return 0;
+  if (!array || array.length === 0) return 0;
+
+  let avg = array.reduce((sum, current) => sum + current, 0) / array.length;
+  return avg;
 }
