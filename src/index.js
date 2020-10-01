139a90a68cbae8bad1b88ebe62b8a80ecacc176a
Mikhail Mudrogelov <mudrogelov.beled@gmail.com>
2020-10-01 19:49:46 +0300

    refactor: format code

--- src/index.js
+++ src/index.js
@@ -1,30 +1,30 @@
 exports.min = function min (array) {
-  let min = 0;
-  
   if (!array || array.length === 0) return 0;
-
-  for (let i = 0; i < array.length; i++) {
-    if (array[i] < min) min = array[i];
+  
+  let min = array[0];
+  for (let element of array) {
+    if (element < min) {
+      min = element
+    }
   }
 
   return min;
 }
 
 exports.max = function max (array) {
-  let max = 0;
-
   if (!array || array.length === 0) return 0;
 
-  for (let i = 0; i < array.length; i++) {
-    if (array[i] > max) max = array[i];
+  let max = array[0];
+  for (let element of array) {
+    if (element > max) {
+      max = element
+    }
   }
-  
   return max;
 }
 
 exports.avg = function avg (array) {
   if (!array || array.length === 0) return 0;
 
-  let avg = array.reduce((sum, current) => sum + current, 0) / array.length;
-  return avg;
+  return array.reduce((sum, current) => sum + current, 0) / array.length;
 }
